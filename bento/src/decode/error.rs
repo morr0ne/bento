9b0216c47db1d75febba27528d3bbe00d5857694
Federico morrone <contact@morrone.dev>
2021-10-28 04:35:08 +0200

    Added error helpers

--- bento/src/decode/error.rs
+++ bento/src/decode/error.rs
@@ -1,13 +1,23 @@
 #[derive(Debug, thiserror::Error)]
 pub enum DecodingError {
     #[error("Missing field {field}")]
-    MissingField {
-        field: String
-    },
+    MissingField { field: String },
     #[error("Unexpected field {field}")]
-    UnexpectedField {
-        field: String
-    },
+    UnexpectedField { field: String },
     #[error("Unknown error")]
     Unknown,
+}
+
+impl DecodingError {
+    pub fn missing_field<T: ToString>(field: T) -> Self {
+        Self::MissingField {
+            field: field.to_string(),
+        }
+    }
+
+    pub fn unexpected_field<T: ToString>(field: T) -> Self {
+        Self::UnexpectedField {
+            field: field.to_string(),
+        }
+    }
 }
