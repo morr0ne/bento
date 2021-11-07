332b31f855f6f37fcc79636a0a3447e0af38bd17
Federico morrone <contact@morrone.dev>
2021-11-07 16:25:51 +0100

    Moved all exports to crate root

--- bento/src/lib.rs
+++ bento/src/lib.rs
@@ -1,7 +1,15 @@
-pub mod decode;
-pub mod encode;
-mod token;
-pub mod value;
+mod decode;
+mod encode;
 
+pub use decode::{Decoder, DecodingError, DictionaryDecoder, FromBencode, ListDecoder, Object};
+pub use encode::{DictionaryEncoder, Encoder, ToBencode};
 #[derive(Debug)]
 pub struct AsString<I>(pub I);
+
+pub(crate) enum Token<'a> {
+    ByteString(&'a [u8]),
+    Integer(&'a [u8]),
+    ListStart,
+    DictionaryStart,
+    End,
+}
