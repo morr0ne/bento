81654ec0f1bab21faee0dea24ad5d263c6bcc47c
Federico morrone <contact@morrone.dev>
2021-11-09 10:36:03 +0100

    Derive common traits and make token public

--- bento/src/lib.rs
+++ bento/src/lib.rs
@@ -3,10 +3,12 @@ mod encode;
 
 pub use decode::{Decoder, DecodingError, DictionaryDecoder, FromBencode, ListDecoder, Object};
 pub use encode::{DictionaryEncoder, Encoder, ToBencode};
-#[derive(Debug)]
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct AsString<I>(pub I);
 
-pub(crate) enum Token<'a> {
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Token<'a> {
     ByteString(&'a [u8]),
     Integer(&'a [u8]),
     ListStart,
