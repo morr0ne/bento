63f3c72f0bf63c0c9dda272a97dfa669db8fc092
Matilde Morrone <contact@morrone.dev>
2024-11-26 13:07:16 +0100

    Crate basic cli interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Install,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Install => {
+            println!("Installing...")
+        }
+    }
 }
